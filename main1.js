027c4b449ff8b253c8abeea2af53f421716a8d18
omda <me8170312@gmail.com>
2025-09-27 10:14:41 +0300

    new

--- main1.js
+++ main1.js
@@ -60,20 +60,20 @@ let c = 'and'
 let d = 'programming'
 console.log(a + " " +b +"\n"+c+" "+d+" ")
 
-console.log(`${a} ${b} 
+console.log(`${a} ${b}
     ${c} ${100*1}`)
     console.log(`${title} ${description} ${date}`)
 console.log('medo')
     */
 
-   /* ***challenge1***
-   let title = "mohamed" ,description = "elzero web school" , date = "1923" ;
-   let abc = `<div class="card">
-   <h3>${title}</h3>
-   <p> ${date}</p>
-   <span>${description}</span>
+/* ***challenge1***
+let title = "mohamed" ,description = "elzero web school" , date = "1923" ;
+let abc = `<div class="card">
+<h3>${title}</h3>
+<p> ${date}</p>
+<span>${description}</span>
 </div>`
-    document.write(abc.repeat(4));
+ document.write(abc.repeat(4));
 
 console.log(2**10)
 console.log(21%8)  //remove5
@@ -179,7 +179,7 @@ console.log(a.indexOf("z"));
 console.log(a.lastIndexOf("l"));
 console.log(a.slice(0,6));                           //not including number og the end
 
-console.log(a.slice(-5,-2)); 
+console.log(a.slice(-5,-2));
 console.log(a.repeat(5));
 
 console.log(a.split(" "));
@@ -191,12 +191,12 @@ console.log(a.substring(-10,4))        //elze   start from zero
 console.log(a.length);
 console.log(a.substring(a.length-5,a.length-3))
 
-console.log(a.substr(0,4))  
+console.log(a.substr(0,4))
 console.log(a.substr(-3))
-console.log(a.substr(-5,3))  
+console.log(a.substr(-5,3))
 console.log(a.includes("web"));
 console.log(a.startsWith("r",4))
-console.log(a.endWith("e",4))      
+console.log(a.endWith("e",4))
 
 //***ch***
 let a = "Elzero Web School";
@@ -243,8 +243,8 @@ if (discount === true ){
         price -= discountAmount +50}
         else{
         price -= discountAmount +30
-        } 
-    } 
+        }
+    }
     else {
         price -= discountAmount +20
     }
@@ -253,10 +253,10 @@ if (discount === true ){
 } else if(country1 === "gaza"){
     price -= 70;
 
-} //else if(student == true ){price -= discountAmount + 20 ;} 
+} //else if(student == true ){price -= discountAmount + 20 ;}
 else {
     price-=10
-} 
+}
 console.log(price)
 */
 /*
@@ -277,17 +277,17 @@ console.log(theGender === "male" ? ("mr") : ("mrs"));
 
 console.log(`"hello" ${theGender === "male" ? ("mr") : ("mrs")} ${theName}`);
 
-theAge < 20 ? console.log(20) : theAge > 20 && theAge <= 60 ? console.log("20 to 60") : theAge > 60 ? console.log("60") : console.log("error") 
+theAge < 20 ? console.log(20) : theAge > 20 && theAge <= 60 ? console.log("20 to 60") : theAge > 60 ? console.log("60") : console.log("error")
 
-let price = null;   //or unfid=fined ,0,"" 
+let price = null;   //or unfid=fined ,0,""
 console.log(`"the price is" ${price || 300} `)
 console.log(`"the price is" ${price ?? 300} `)   //null or undefined only
-//console.log("price = "+price) 
+//console.log("price = "+price)
 
 //*****ch if******
-let a = 
+let a =
 a < 10 ? console.log("10") : a >= 10 && a <=40 ? console.log("10 to 40") : a>40 ? console.log(">40") : console.log("unknown");
-let st = "Elzero Web School" 
+let st = "Elzero Web School"
 if ((2* st.length).toString() === "34"){
     console.log("good")
 };
@@ -333,14 +333,14 @@ switch (job){
     break;
     case "support" :
         salary = 4000;
-        break; 
+        break;
         case "it" :
             salary = 4000;
-            break; 
-            case "developer" : 
+            break;
+            case "developer" :
             case "designer" :
             salary = 7000;
-            break; 
+            break;
             default :
             salary =2000;
             break;
@@ -357,7 +357,7 @@ if (holidays === 0){
 money = 4000
 } else if (holidays === 3){
     money = 2000;
-} 
+}
 else{
     money = 1000;
 }
@@ -370,9 +370,9 @@ let myfriends = ["Ahmed" , "Mohamed" , "Hazem", ["Marwan" ,"Hazem"]];
 console.log(`hello ${myfriends[3][1][3]}`)
 
 console.log(`${myfriends}`);
- myfriends [1] = "mozza";  
+ myfriends [1] = "mozza";
 console.log(`${myfriends}`);
-myfriends [3][1] = "medo";  
+myfriends [3][1] = "medo";
 console.log(`${myfriends}`);
 
 let myfriends = ["Ahmed" , "Mohamed" , "Hazem", "Marwan" ,"maryem"] ;
@@ -385,7 +385,7 @@ myfriends[myfriends.length] = "mohamedi"
 
 console.log(myfriends);
 console.log(myfriends.length)
-*/
+
 
 let myfriends = ["Ahmed" , "Mohamed" , "Hazem"] ;
 console.log(myfriends);
@@ -396,6 +396,7 @@ myfriends.push("rere" , "lolo");
 
 console.log(myfriends)
 //myfriends.shift()
+
 let first =myfriends.shift();
 
 console.log(myfriends);
@@ -408,4 +409,504 @@ console.log(`my last name is ${last}`)
 
 console.log(myfriends);
 console.log(`my last name is ${last}`)
-//omda
+
+
+let myfriends = ['ahmed','mohamed','hazem','sayed','ahmed'];
+console.log(myfriends);
+console.log(myfriends.indexOf('ahmed',2));
+console.log(myfriends.lastIndexOf('ahmed',-2));
+console.log(myfriends.indexOf('omda'));
+console.log(myfriends.includes('omda'));  //false
+
+if (myfriends.includes('ahmed')){
+    console.log('yes')
+}
+
+if (myfriends.indexOf('ahmed')===0){
+    console.log('yes')
+}
+
+if (myfriends.lastIndexOf('osama')===-1){
+    console.log('not found')
+}
+
+myfriends = [10, 20 ,"40", "200","ahmed" ,"sayed", "medo",-10 ,-20]
+console.log(myfriends.sort());
+console.log(myfriends.reverse())
+console.log(myfriends.sort().reverse());
+
+
+//slice,splice
+
+let myfriends =["ahmed","mohamed","moza","zoza","maryem"]
+console.log(myfriends);
+console.log(myfriends.slice(1,4));
+console.log(myfriends.slice(-3,4));
+console.log(myfriends.slice(-4,-1));
+
+console.log(myfriends);
+myfriends.splice(1,3,"omda","hesham")
+console.log(myfriends)
+
+
+let myfriends =["ahmed","mohamed","moza","zoza","maryem"]
+let names= ["sameh","hoda"];
+let friends=["medo","dalia"];
+//let allfriends =myfriends.concat(names).concat(friends);
+let allfriends =myfriends.concat(names,friends,"jjjj",[1,3]);
+
+console.log(allfriends)
+console.log(allfriends.join(" @ "));
+console.log(allfriends.join(" | "));
+
+
+//array challenge
+
+let zero = 0;
+let counter = 3;
+let my = ["Ahmed","mazero","elham","Osama","Gamal","Ameer"];
+console.log(my);
+//my = my.reverse().slice(counter-1);
+console.log(my.slice(zero,counter).concat(my[counter]).reverse());  //ch1
+
+console.log(my.slice(my.indexOf("mazero"),counter).reverse());       //2
+
+console.log(my[--counter].slice(zero,counter).concat(my[++zero].slice(counter)));
+
+//console.log(my[++].my[]slice(-(++counter)).toUpperCase())
+console.log(my[zero][4]+my[1][5].toUpperCase());   //rO
+console.log(zero);
+console.log(counter)
+*/
+
+// *******************************loops*************************************
+/*
+for (let i = 1; i<12;i++) {
+    console.log(i)
+}
+
+let myfriends =["osama",1,2,3,"medo","hoss","eslam","hazem"];
+let names=[];
+
+if(typeof myfriends[0]==='string'){
+    console.log(myfriends[0]);
+    names.push(myfriends[0])
+
+    if(typeof myfriends[1]==='string'){
+    console.log(myfriends[1]);
+        names.push(myfriends[1])
+
+}
+
+if(typeof myfriends[4]==='string'){
+    console.log(myfriends[4])
+        names.push(myfriends[4])
+
+}
+}
+console.log(names)
+
+for(i=0;i<myfriends.length;i++){
+    if(typeof myfriends[i]==='string'){
+    console.log(myfriends[i]);
+     names.push(myfriends[i])
+
+}}
+console.log(names)
+
+
+//for(i=0 ; i<myfriends.length ; i++){
+//    console.log(myfriends[i]);
+//}
+
+
+let products =["bannana","apple","juice","shop"];
+let colors = ["red","black","green"];
+let models = [2020,2021];
+
+for(i=0;i<products.length;i++){
+    console.log(products[i])
+    //console.log("************************************")
+    for(k=0;k<colors.length;k++){
+        console.log("-"+colors[k])
+    }
+    for(s=0;s<models.length;s++){
+        console.log("-"+models[s])
+    }
+}
+
+let products =["bannana","apple","juice","shop"];
+let colors = ["red","black","green"];
+mainloop: for(i=0 ;i<products.length;i++){
+    console.log(products[i])
+nestedloop:    for(j=0;j<colors.length;j++){
+    console.log(colors[j]);
+        if (colors[j]==="black"){
+        break mainloop;
+    }
+}
+}
+
+let products =["bannana","apple","juice","shop","mouse","kryboard"];
+let i=0;
+for(;;){
+console.log(products[i])
+    i+=2;
+ if (i===products.length){
+break;
+}
+}
+
+let products =["bannana","apple","juice","shop","mouse","kryboard"];
+let colors = ["red","black","green"];
+let showcount = 5
+document.write(`<h1> this is ${showcount} products</h1>`)
+for (i=0;i<showcount;i++){
+    document.write(`<div><h3>${products[i]}</h3></div>`)
+    for(j=0;j<colors.length;j++){
+        document.write(`<div><p>${colors[j]}</p></div>`)
+    }
+        document.write(`<div><p>${colors.join(" | ")}</p></div>`)
+}
+
+
+let products =["bannana","apple","juice","shop","mouse","kryboard"];
+let i =0
+while (i<products.length){
+    console.log(products[i])
+    i++
+}
+
+
+let products =["bannana","apple","juice","shop","mouse","kryboard"];
+let i =0
+do{
+    console.log(i);
+    i+=1
+}    while(false);
+
+    console.log(i)
+
+*/
+
+
+/*
+//****************************loop challenge****************
+
+let myAdmins =["Ahmed","Osama","Sayed","Stop","Samara","safckjsbfk","sjkdfsd"];
+let myEmployes=["Amgad","Samah","Ameer","Omar","Osman","Amany","Smaya","Osama","Asel","aseeel","soso","OOOOOOOOOOO"];
+for (i=0;i<myAdmins.length;i++){
+    if (myAdmins[i]==="Stop"){
+        break;
+    }
+}
+X=1
+z=1
+f=1
+h=1
+document.write(`<div> we have ${i} admins </div>`);
+counter=1
+
+document.write(`<div>the admin for team ${X} is Ahmed</div>`)
+
+document.write(`<div><h2>team members:</h2></div>`)
+for(counter;counter<myEmployes.length;counter++){
+    if (myEmployes[counter][0]=== "A"){
+document.write(`<div><p>${z++} ${myEmployes[counter]}</p></div>`)
+}}
+//2 problems  1 is ahmed , 1,2,3 members
+
+d=1
+document.write(`<div>the admin for team ${X+=1} is Osama</div> <h2>team members : </h2>`)
+for(d;d<myEmployes.length;d++){
+    if (myEmployes[d][0]=== "O"){
+document.write(
+`<div>
+    <p>${f++}${myEmployes[d]}</p>
+
+</div>`)
+}}
+
+
+s=1
+
+document.write(`<div>the admin for team ${X+=1} is Osama</div> <h3>team members : </h3>`)
+for(s;s<myEmployes.length;s++){
+    if (myEmployes[s][0]=== "S"){
+document.write(
+`<div>
+    <p>${h++}${myEmployes[s]}</p>
+
+</div>`)
+}}
+
+console.log(X)
+
+*/
+
+
+//****************************function********************************
+/*
+console.log("osama")
+        console.log("hello ahmed")
+    console.log("hello fjfd")
+
+function s(userName, age){
+    if(age <20){
+        console.log("error")
+    }    else{
+                console.log("hi "+ userName + " your age is " + age)
+        }
+    }
+s("ahmed",16)
+s("samya",4)
+s("jdfnjd",36)
+
+function generateYears (start , end ,exclude)
+{
+for(i=start;i<=end;i++){
+    if (i===ex){
+    continue;
+
+}
+    console.log(i)
+}
+}
+generateYears(22,45,25);
+
+
+function x(userName){
+    return "hello " + userName
+}
+result=x("medo")
+console.log(result)
+
+function calc(num1,num2){
+    return num1+num2+100                 //exec
+}
+result=calc(10,50)
+console.log(result)
+
+function generate(start , end){
+for(i=start;i<=end;i++){
+    if(i===17){
+        return ;
+
+    }
+
+    console.log(i)
+}
+}
+generate(10 ,20)
+
+
+function c(userName = "mohsen",age ="error"){
+//    if(age===undefined){
+//        age ="unknown"
+//
+//    }
+return "your name is "+ userName + " your age " + age
+}
+//console.log(c("mohsen",27))
+console.log(c())
+
+//next lesson 61
+function calc(...number){
+    result=0;
+for(i=0;i<number.length;i++){
+    //console.log(number[i])
+     result+=number[i];
+}
+return `final result is ${result}`;
+}
+console.log(calc(10,30,40,30,20))
+//console.log(result)
+
+
+
+function x(name= "un" , age="unk" , ti="un" ,show="yes" , ...sk){
+document.write(`<div>`)
+document.write(`<h2> your name is ${name}</h2>`)
+document.write(`<h2> your age is ${age}</h2>`)
+document.write(`<h2> your time is $${ti}</h2>`)
+if (show==="yes"){
+    if(sk.length>0){
+         document.write(`<p> skills: ${sk.join(" | ")}</p>`)
+}
+else{
+     document.write(`<p> no skills </p>`)
+
+
+}
+    //document.write(`<p> skills exists</p>`)
+}    else{
+         document.write(`<p> skills is hidden</p>`)
+
+    }
+
+
+document.write(`</div>`)
+
+}
+x("medo",19,20,"yes","prog","script")
+
+
+function z (a, b ,c){
+    document.write(`<div>`)
+    for(i=0;i<z.length;i++){
+        if (typeof z[i]==='string'){
+            document.write(`<p>${z[i]}</p>`)
+            console.log(z[i])
+        }
+
+    }
+//document.write(`<p>${a}</p>`)
+
+    document.write(`</div>`)
+
+}
+z("medo",56,true)
+
+
+Result = function (n1,n2){
+    return (n1+n2)
+}
+function say(){
+    console.log("hello zzzzzmedo")
+}
+//console.log(Result(2,16))
+document.getElementById("show").onclick = say
+//document.getElementById("show").onclick = function () {console.log("omda")}
+setTimeout(function () {
+console.log("good")
+}, 2000)
+*/
+/*
+//example 1
+
+function saymessage (fname,lname){
+    message = "hello"
+    function concatMsg (){
+        message = `${message} ${fname} ${lname}`
+    }
+concatMsg()
+return message
+}
+console.log(saymessage("omda","medo"))
+*/
+
+/*
+//example 2
+
+function saymessage (fname,lname){
+    message = "hello"
+    function concatMsg (){
+        return `${message} ${fname} ${lname}`
+    }
+
+return concatMsg()
+}
+console.log(saymessage("omda","medo"))
+*/
+/*
+//example 3
+
+function saymessage (fname,lname){
+    message = "hi"
+
+    function concatMsg (){
+        function fulname (){
+            return `${fname} ${lname}`
+
+        }
+
+        return `${message} ${fulname()}`
+    }
+
+return concatMsg()
+}
+console.log(saymessage("mohamed","emad"))
+*/
+
+//arrow function
+/*
+let print = function () {
+    return 20
+}
+console.log(print())
+
+let prin = _ => 10
+console.log(prin())
+
+let pri = (num) => num
+console.log(pri(33))
+
+let prit = (num1, num2) => num1 + num2
+console.log(prit(30, 40))
+*/
+/*
+x = 10
+if (true) {
+    //let x = 50
+    console.log(x)
+}
+console.log(x)
+
+a = 2
+b = 4
+function x() {
+    a = 4
+    b = 3
+    console.log(`gunction global ${a} ${b}`)
+}
+console.log(`from global ${a} ${b}`)
+x()
+
+let x = 50
+function parent() {
+
+    function child() {
+        console.log(x * 10)
+    }
+    function grand() {
+        let b = 100
+        console.log(x + 20)
+        console.log(b)
+    }
+
+    grand()
+    child()
+}
+parent()
+*/
+let mynums = [1, 3, 4, 5, 6, 7]
+let newnums = []
+for (i = 0; i < mynums.length; i++) {
+    newnums.push(mynums[i] + mynums[i])
+}
+console.log(newnums)
+
+
+//map
+/*
+let addself = mynums.map(function (element,index,array){
+  //  console.log("current element="+element)
+    //console.log(`current index= ${index}`)
+    //console.log("current array="+array)
+    return element+element+element;
+}
+)
+*/
+
+
+//                   with arrow function
+
+let addself = mynums.map((a) => a + a + a)
+console.log(addself)
+
+//way3
+function addition(element) {
+    return element + element
+
+}
+console.log(mynums.map(addition))
